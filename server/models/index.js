6933c8686309b872b77c306a81beb5a1f137a062
Oscar Carvente <oscar.carvente@gmail.com>
2023-08-02 17:51:37 -0700

    Revised purchase and sales routes

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ module.exports = {
       const listingsInCart = await db.any('SELECT listings.* ,json_agg(listing_photos.*) AS photos FROM listings JOIN carts ON listings.id = carts.listing_id LEFT JOIN listing_photos ON listings.id = listing_photos.listing_id WHERE carts.user_id = $1 GROUP BY listings.id', [user_id]);
       return listingsInCart;
     } catch(error) {
-      console.log('unable to retrieve listings, ', error);
+      console.log('unable to retrieve cart, ', error);
     }
   },
   createItemInCartDB: async(user_id, listing_id) => {
@@ -24,9 +24,9 @@ module.exports = {
       const insertTransaction = await db.any('INSERT INTO transactions (buyer_id, total_amount) VALUES ($1, $2)', [buyer_id,total_amount ]);
       const transaction_id  = await db.any('SELECT id FROM transactions WHERE buyer_id = $1', [buyer_id]);
       const updateListing = await db.any(`UPDATE listing SET transaction_id = $1, status = $2 WHERE id = ANY($3)`, [transaction_id, status, listing_ids]);
-      return [insertTransaction, updateListing];
+      return updateListing;
     } catch(error) {
-      console.log('unable to retrieve listings, ', error);
+      console.log('unable to create listings, ', error);
     }
   },
 
@@ -36,13 +36,13 @@ module.exports = {
       console.log(transactions);
       return transactions;
     } catch(error) {
-      console.log('unable to retrieve listings, ', error);
+      console.log('unable to retrieve transactions, ', error);
     }
   },
 
   getPurchasesByUserIdFromDB: async(user_id) => {
     try {
-      const purchases  = await db.any('SELECT listings.* FROM listings JOIN transactions WHERE transactions.id = listings.transactions_id');
+      const purchases  = await db.any('SELECT listings.* FROM listings JOIN transactions ON transactions.id = listings.transaction_id WHERE transactions.buyer_id = $1', [user_id]);
       return purchases;
     } catch(error) {
       console.log('unable to retrieve purchases, ', error);
@@ -51,7 +51,7 @@ module.exports = {
 
   getSalesByUserIdFromDB: async (user_id) => {
     try {
-      const sales = await db.any('SELECT * from listings where seller_id = $1 AND status = $2',[user_id, 'fulfilled'] )
+      const sales = await db.any('SELECT * from listings where seller_id = $1',[user_id] )
       return sales;
     } catch(error) {
       console.log('unable to retrieve sales, ', error);
